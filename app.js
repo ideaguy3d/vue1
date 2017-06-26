560790184baf755b47b1f9d487525bfc63f02ab1
Julius Alvarado <angularjs.uiux@gmail.com>
2017-06-25 21:22:02 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+/**
+ * Created by Julius Alvarado on 6/25/2017.
+ */
+
+(function () {
+    "use strict";
+
+    var growler = new Vue({
+        el: '#growler',
+        data: {
+            appLink: '<a href="http://julius3d.com">Julius3D Studios</a>',
+            appSlogan: 'User Centered Software Development',
+            appLogo: 'http://julius3d.com/img/logo/j3d.png',
+            appStyle: {
+                color: '#434343',
+                fontWeight: 'bolder',
+                border: 'solid 2px cornflowerblue',
+                display: 'inline-block',
+                padding: '8px'
+            }
+        }
+    });
+
+}());
+
+//
